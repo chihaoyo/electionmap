7a1c9ee7f0f50591e361868eb2c963757366ef1a
chihaoyo <chihaoyo@gmail.com>
2015-12-11 18:46:46 +0800

    Added borough data

---
+++ js/interactivity.js
@@ -0,0 +1,45 @@
+var $bigmap = $('#bigmap');
+var $citymap = $('#citymap');
+var $cities = $citymap.find('.city');
+var $info = $citymap.find('#district-info');
+
+var interactiveCities = ['TPE','NTC','TYN','ZMI','TXG','CHW','NAN','YLN','CYI','TNN','KHH','PIF'];
+for(city of interactiveCities) {
+	$bigmap.find('#cities > #' + city).click(function() {
+		var $this = $(this);
+		var thisCity = $this.attr('id');
+		$this.attr('class', 'active')
+		$citymap.show();
+		$cities.filter('#' + thisCity).show();
+	});
+	$cities.filter('#' + city).find('> [id^="' + city + '"]:not([id$="disabled"])').hover(function() {
+		var ids = $(this).attr('id').split('-');
+		var thisCity = ids[0];
+		var thisDistrict = parseInt(ids[1]);
+
+		var city = db[thisCity];
+		var district = city.districts[thisDistrict - 1];
+		var districtName = city.name + '第' + thisDistrict + '選區';
+		var districtDetail = '';
+		for(subdistrict of district.subdistricts) {
+			var html = '<div class="subdistrict">' + subdistrict + '</div>';
+			if(subdistrict.indexOf('：') != -1) {
+				// a subdistrict with more details
+				var buffer = subdistrict.split('：');
+				subdistrict = buffer[0];
+				buffer = buffer[1].split('、');
+				html = '<div class="subdistrict"><div>' + subdistrict + buffer.length + '里</div>' + '<div class="boroughs">' + buffer.map(function(v) { return '<div class="borough">' + v + '</div>'; }).join('') + '</div></div>'
+			}
+			districtDetail += html;
+		}
+		$info.find('#name').html(districtName);
+		$info.find('#detail').html(districtDetail);
+		$info.show();
+	});
+}
+$citymap.find('#close').click(function() {
+	$bigmap.find('.active').attr('class', '');
+	$info.hide();
+	$cities.hide();
+	$citymap.hide();
+});
